1d341c4f1e4b1c9c9591681eba8b0c184c1cd5d5
Diego Soares de Oliveira <104038378+DiegoOliveiraa@users.noreply.github.com>
2023-08-01 13:44:54 -0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,54 +1,48 @@
 package src
 
-class main {
+class Main {
     enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
     data class Usuario(val nome: String, val nivel: Nivel, val materia: ConteudoEducacional, val formacao: Formacao)
 
-    data class ConteudoEducacional(var nome: String, val duracao: Int)
+    data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-    data class Formacao(val nome: String, var conteudos: String) {
+    data class Formacao(val nome: String, val conteudos: MutableList<ConteudoEducacional>) {
         val inscritos = mutableListOf<Usuario>()
-        val lista: MutableList<Usuario> = inscritos
 
         fun matricular(usuario: Usuario) {
             inscritos.add(usuario)
         }
 
-        fun getSysList(): MutableList<Usuario> {
-            return lista
+        fun getInscritos(): List<Usuario> {
+            return inscritos
         }
     }
 
     fun main() {
-
-        val direito = Formacao("direito", "é us guri")
-        val professor = Formacao("professor", "é us gu")
-        val quimico = Formacao("quimico", "é us ri")
-
+        val direito = Formacao("direito", mutableListOf())
+        val professor = Formacao("professor", mutableListOf())
+        val quimico = Formacao("quimico", mutableListOf())
 
         // Matérias
-
         val portugues = ConteudoEducacional("português", duracao = 30)
         val matematica = ConteudoEducacional("matematica", duracao = 40)
         val ciencias = ConteudoEducacional("ciencias", duracao = 50)
 
-        //Alunos
-
+        // Alunos
         val usuario1 = Usuario("Diego", Nivel.BASICO, ciencias, quimico)
         val usuario2 = Usuario("Luiza", Nivel.INTERMEDIARIO, matematica, professor)
         val usuario3 = Usuario("Isaac", Nivel.DIFICIL, portugues, direito)
 
-        //tempo de aula
+        // Tempo de aula
         println(usuario2.materia.duracao)
 
-        //Matricula
-        usuario1.formacao.matricular(usuario1)
-        usuario2.formacao.matricular(usuario2)
-        usuario3.formacao.matricular(usuario3)
+        // Matricula
+        direito.matricular(usuario1)
+        professor.matricular(usuario2)
+        quimico.matricular(usuario3)
 
-        //Chamada do Diego
-        println(usuario1.formacao.getSysList())
-
+        // Chamada do Diego
+        println(direito.getInscritos())
     }
 }
